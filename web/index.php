9a062f0c18cbfdefd7d8015a6021e2c43a57564d
Kimt Simin <kimtonlines@gmail.com>
2018-07-31 22:52:23 +0000

    ok commit

--- web/index.php
+++ web/index.php
@@ -35,26 +35,26 @@ $botman = BotManFactory::create($config);
 // Give the bot something to listen for.
 $botman->hears("bonjour", function (BotMan $bot) {
   $bot->reply(Question::create("Quick replie 3:")->addButtons([
-      Button::create("Acheteur")->value("Acheteur"),
+      Button::create("Acheteur")->value("acheteur"),
       Button::create("Coopérative")->value("Coopérative"),
       Button::create("Agriculteur")->value("Agriculteur"),
   ]));
 });
 
-$botman->hears("Acheteur", function (BotMan $bot) {
+$botman->hears("acheteur", function (BotMan $bot) {
     $bot->reply(Question::create("Choix:")->addButtons([
-        Button::create("1-Déposer une annonce")->value("Deposer"),
+        Button::create("1-Déposer une annonce")->value("deposer"),
         Button::create("2-Voir toutes les annonces")->value("2-Voir toutes les annonces"),
     ]));
 });
 
-$botman->hears("Deposer", function (BotMan $bot) {
+$botman->hears("deposer", function (BotMan $bot) {
     $bot->reply(Question::create("Choix:")->addButtons([
         Button::create("1-Titre de votre annonce")->value("1-Titre de votre annonce"),
         Button::create("2-Description de l'annonce")->value("2-Description de l'annonce"),
         Button::create("3-Prix au kilo")->value("3-Prix au kilo"),
         Button::create("4-Quel est votre budget?")->value("4-Quel est votre budget?"),
-        Button::create("5-Choisir une catégorie")->value("5-Choisir une catégorie"),
+        Button::create("5-Choisir une catégorie")->value("categorie"),
     ]));
 });
 
