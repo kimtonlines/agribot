90c4a872955cab78a52a18098140e3451f91fe93
Kimt Simin <kimtonlines@gmail.com>
2018-07-31 18:23:16 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kimt
+ * Date: 31/07/2018
+ * Time: 18:15
+ */
+
+require_once __DIR__.'vendor/autoload.php';
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+
+$config = [
+    'facebook' => [
+        'token' => 'EAAc7UhlqCQABAJNbh6YQ3XUHsfhnVPa6dZCEx13InSEmghTzPTBWWEqLd7CVSslSkBKcjs8WwEmdtZCX13qjpW60NE44dvoLJKDm0NCZCKGYq8h5yRuqIKe7kC5524ApKGQNhJUHUVXPxjI8luzebYLZBcWctoxK1kDZAShcZAaBKjoVdB68ga',
+        'app_secret' => '3f326ba22a1958a9698e2c194ded7ef1',
+        'verification'=>'3f326ba22a1958a9698e2c194ded7ef1',
+    ]
+];
+
+DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
+
+// Create BotMan instance
+BotManFactory::create($config);
+
+// Give the bot something to listen for.
+$botman->hears('bonjour', function (BotMan $bot) {
+    $bot->reply('Bonjour à vous.');
+});
+
+// Start listening
+$botman->listen();
