2591fefc1c1b53f29265e1bf2c41a716e569a8a5
Kimt Simin <kimtonlines@gmail.com>
2018-08-01 11:49:30 +0000

    RAS

--- src/OnboardingConversation.php
+++ src/OnboardingConversation.php
@@ -9,7 +9,6 @@
 namespace AgriBot;
 
 
-use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Messages\Conversations\Conversation;
 use BotMan\BotMan\Messages\Incoming\Answer;
 use BotMan\BotMan\Messages\Outgoing\Actions\Button;
@@ -55,32 +54,16 @@ class OnboardingConversation extends Conversation
             // Save result
             $budget = $answer->getText();
 
-            $this->askCtegorie();
+            //$this->askCtegorie();
+            $this->stopsConversation("Choisir une catégorie?");
         });
     }
 
         public function askCtegorie()
     {
         $this->ask('Choisir une catégorie?', function(Answer $answer) {
-            $this->bot->hears("ok", function (BotMan $bot) {
-                $bot->reply(ListTemplate::create()
-                    ->useCompactView()
-                    ->addGlobalButton(ElementButton::create('voir plus')->url('http://agrivoire.herokuapp.com/sol-culture'))
-                    ->addElement(
-                        Element::create('Produit de saison')
-                            ->image('https://agrivoire.herokuapp.com/public/pages/July2018/sack-309849_640-cropped.png')
-                            ->addButton(ElementButton::create('Plus')
-                                ->payload('psaison')->type('postback'))
-                    )
-                    ->addElement(
-                        Element::create('Produit hors saison')
-                            ->image('https://agrivoire.herokuapp.com/public/pages/July2018/SugarCane-cropped.png')
-                            ->addButton(ElementButton::create('Plus')
-                                ->payload('psaison')->type('postback'))
-                    )
-                );
-            });
-
+            // Save result
+            $budget = $answer->getText();
             //$this->say($description);
         });
     }
